4854ebd18ff4e9e8badc95eee68e08abd1917966
Marshall Warners <m@marshallwarners.com>
2025-11-12 21:33:05 -0500

    Binary sensor is_on property

--- maestro/domains/binary_sensor.py
+++ maestro/domains/binary_sensor.py
@@ -4,3 +4,7 @@ from maestro.integrations.home_assistant.domain import Domain
 
 class BinarySensor(Entity):
     domain = Domain.BINARY_SENSOR
+
+    @property
+    def is_on(self) -> bool:
+        return self.state == "on"
