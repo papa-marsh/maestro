ba71dc33d27c5aecfc662a6b678b115defad15a6
Marshall Warners <m@marshallwarners.com>
2025-12-30 13:21:19 -0500

    Support multiple calendars for get_events

--- maestro/domains/calendar.py
+++ maestro/domains/calendar.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from maestro.domains.entity import Entity
 from maestro.integrations.home_assistant.domain import Domain
+from maestro.integrations.home_assistant.types import EntityId
 
 
 class Calendar(Entity):
@@ -17,10 +18,12 @@ class Calendar(Entity):
         start_date_time: datetime | None = None,
         end_date_time: datetime | None = None,
         duration: dict[str, int] | None = None,
+        calendars: list[EntityId] | None = None,
     ) -> dict[str, Any]:
         """
         Use only one of start/end_date_time or duration.
         Duration example: {"hours": 48} or {"days": 7}.
+        Pass multiple calendar entity IDs to return events from multiple calendars.
         """
         if duration and any([start_date_time, end_date_time]):
             raise ValueError("Action get_events accepts start/end dates or duration, but not both")
@@ -33,5 +36,12 @@ class Calendar(Entity):
             kwargs = {"start_date_time": start_date_time, "end_date_time": end_date_time}
         else:
             raise ValueError("Action get_events must be passed start/end dates or duration")
+
+        self.state_manager.hass_client.perform_action(
+            domain=self.domain,
+            action="get_events",
+            entity_id=[str(c) for c in calendars] if calendars else str(self.id),
+            **kwargs,
+        )
 
         return self.perform_action("get_events", **kwargs, response_expected=True)
