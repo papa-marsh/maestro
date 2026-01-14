60b862f3cff99a7caf99aebd5580239563da0ea7
Marshall Warners <m@marshallwarners.com>
2026-01-14 15:47:04 -0500

    add is_home property to DeviceTracker

--- maestro/domains/device_tracker.py
+++ maestro/domains/device_tracker.py
@@ -1,7 +1,11 @@
-from maestro.domains.entity import Entity
+from maestro.domains.entity import HOME, Entity
 from maestro.integrations.home_assistant.domain import Domain
 
 
 class DeviceTracker(Entity):
     domain = Domain.DEVICE_TRACKER
     allow_set_state = False
+
+    @property
+    def is_home(self) -> bool:
+        return self.state == HOME
